8f00c3035dc5b06401162f57363c7e7b2f0e9b79
widowild <widomaker2k7@gmail.com>
2011-05-31 21:09:33 +0200

    add licences gplv3, del comments

--- obreddit.py
+++ obreddit.py
@@ -1,4 +1,18 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+#
+# obreddit [version 0.1]
+#
+# Copyright 2010 Wido <widomaker2k7@gmail.com>
+# Distributed under the terms of the GNU General Public License v3.
+# See http://www.gnu.org/licenses/gpl.txt for the full license text.
+#
+# Depends : Openbox and Python 3
+#
+# lets see your profile on the site http://www.reddit.com with Menu Openbox
+#
+# Change your username in /home/$USER/.config/obreddit.conf
+# add pipemenu in your menu and add action for example
+# python /your_folder/obreddit.py
 
 import os
 import urllib.request
@@ -8,14 +22,14 @@ import sys
 
 conf = os.environ['XDG_CONFIG_HOME'] + "/obreddit.conf"
 
-if not os.path.isfile(conf): # test existence d'un fichier
+if not os.path.isfile(conf):
     print("change your username in", conf)
     config = configparser.ConfigParser()
     config['DEFAULT'] = {'User' : 'yourusername'}
     with open(conf, 'w') as configfile:
         config.write(configfile)
 
-# lire fichier de configuration
+# read configuration file
 config = configparser.ConfigParser()
 config.read(conf)
 user = config['DEFAULT']['User']
@@ -23,7 +37,6 @@ user = config['DEFAULT']['User']
 url = "http://www.reddit.com/user/" + user + "/about.json"
 raw = urllib.request.urlopen(url).read().decode('utf-8')
 data = json.loads(raw)['data']
-#print data
 karma = data['link_karma']
 comment = data['comment_karma']
 
